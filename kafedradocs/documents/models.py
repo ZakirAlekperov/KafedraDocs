024d20a4992ef75d9dd0c481c687aa72c8095448
zakir <zakiralekperov1998@gmail.com>
2024-09-17 19:00:48 +0300

    Завершен класс Documents в приложении documents

--- kafedradocs/documents/models.py
+++ kafedradocs/documents/models.py
@@ -4,5 +4,16 @@ from django.db import models
 
 class Documents(models.Model):
     title:str = models.CharField(max_length=255, help_text="Заголовок документа")
+    documentt_type:str = models.CharField(max_length=100, help_text="Тип документа (учебный план, отчет, и т. д.)")
+    created_at:models.DateTimeField = models.DateTimeField(auto_now_add=True, help_text="Дата создания документа")
+    updated_at:models.DateTimeField = models.DateField(auto_now_add=True, help_text="Дата последнего обновления документа")
+    file:str = models.FileField(upload_to='documents/', help_text="Файйл документа")
+    
+    
+    def __str__(self) -> str:
+        return self.title
+    
+
+
 
 
