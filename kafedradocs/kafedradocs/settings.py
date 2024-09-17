8a2f184b2238ed46f20006b17ff3b90e81e0287a
zakir <zakiralekperov1998@gmail.com>
2024-09-17 18:10:56 +0300

    Создано приложение  'courses_app', 'users_app', 'reports_app', 'settings_app'

--- kafedradocs/kafedradocs/settings.py
+++ kafedradocs/kafedradocs/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'documents',
+    'courses_app',
+    'users_app',
+    'reports_app',
+    'settings_app',
+
+
 ]
 
 MIDDLEWARE = [
