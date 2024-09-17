8fcda51fd10e305f1ea42f7762a3eba1ce4cb9d8
zakir <zakiralekperov1998@gmail.com>
2024-09-17 20:10:30 +0300

    зарегестрированы модели в documents

--- kafedradocs/documents/admin.py
+++ kafedradocs/documents/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Document, Category, DocumentVersion
 
-# Register your models here.
+
+@admin.register(Document)
+class DocumentAdmin(admin.ModelAdmin):
+    list_display = ('titile', 'document_type', 'created_at', 'updated_at')
+    search_fields = ('title', 'document_type')
+    
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name')
+    search_fields = ('name')
+
+
+@admin.register(DocumentVersion)
+class DocumentVersionAdmin(admin.ModelAdmin):
+    list_display = ('document', 'version_number', 'created_at')
+    search_fields = ('document__titile')
--- kafedradocs/documents/models.py
+++ kafedradocs/documents/models.py
@@ -3,18 +3,34 @@ from django.db import models
 # Create your models here.
 
 class Document(models.Model):
+    """
+        Назначение: Хранит информацию о документах кафедры.
+        
+        Поля:
+            - title: Заголовок документа.
+            - document_type: Тип документа (например, "учебный план", "отчет").
+            - created_at: Дата создания документа.
+            - updated_at: Дата последнего обновления документа.
+            - file: Файл документа.
+    """
     title:str = models.CharField(max_length=255, help_text="Заголовок документа")
     documentt_type:str = models.CharField(max_length=100, help_text="Тип документа (учебный план, отчет, и т. д.)")
     created_at:models.DateTimeField = models.DateTimeField(auto_now_add=True, help_text="Дата создания документа")
     updated_at:models.DateTimeField = models.DateField(auto_now_add=True, help_text="Дата последнего обновления документа")
     file:str = models.FileField(upload_to='documents/', help_text="Файйл документа")
     
-    
+    """"""
     def __str__(self) -> str:
         return self.title
     
 
 class Category(models.Model):
+    """
+        Позволяет классифицировать документы по категориям.
+        
+        Поля:
+            - name (str): Название категории.
+    """
     name:str = models.CharField(max_length=100, help_text="Название категории")
     
     def __str__(self) -> str:
@@ -22,6 +38,16 @@ class Category(models.Model):
 
 
 class DocumentVersion(models.Model):
+    """
+        Позволяет отслеживать версии документов.
+
+        Поля:
+            - document (ForeignKey): Связь с моделью Document.
+            - version_number (int): Номер версии документа.
+            - file (FileField): Файл версии документа.
+            - created_at (datetime): Дата создания версии.
+    """
+    
     document:Document = models.ForeignKey(Document, on_delete=models.CASCADE, related_name='versions', help_text='Документ для версии')
     version_number:int = models.IntegerField(help_text="Номер версии документа")
     file:str = models.IntegerField(upload_to='documents_version/', help_text="Файл версии документа")
